e7bec0e6aceb31a44dfc43a886bffbda6808e8a5
Chris Brulak <chris.brulak@ama.ab.ca>
2012-06-11 16:21:43 -0600

    renaming paramters to be more clear

--- lib/mime_version.rb
+++ lib/mime_version.rb
@@ -1,24 +1,22 @@
 class MimeVersion
   def initialize(app)
-    @default_version = app
-    #@default_version = default_version
+    @app = app
   end
 
   def self.parse_version(content_type)
     tag = "version="
     @version = @default_version
-    mime_types = content_type.split(";")
     @hash = Hash[content_type.split(';').map { |i| i.split('=') }]
-    @version = @hash["version"] || @default_version.to_s
+    @version = @hash["version"]
   end
 
   def call(env)
 
-    ENV['version'] = @default_version.to_s
+    ENV['version'] = ""
     if env.has_key? "CONTENT_TYPE"
       @version = MimeVersion.parse_version(env["CONTENT_TYPE"])
       ENV['version'] = @version
     end
-  @default_version.call(env)
+  @app.call(env)
   end
 end
